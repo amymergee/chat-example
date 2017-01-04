9f77ab7d4bf6413d626b57dbaeddb503f77e82cf
Amy Rule <arule@youthlightbooks.com>
2017-01-04 10:00:39 -0500

    Added port variable

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var io = require('socket.io'),
   connect = require('connect'),
   chatter = require('chatter');
-
+var port = process.env.PORT || 3000;
 //var app = connect().use(connect.static('public')).listen(3000);
-var app = connect().use(connect.static('public')).listen();
+var app = connect().use(connect.static('public')).listen(port);
 var chat_room = io.listen(app);
 
 chatter.set_sockets(chat_room.sockets);
