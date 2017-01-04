9a3d232646af908b9758e0c4d086f1e9eae620d0
Amy Rule <arule@youthlightbooks.com>
2017-01-04 13:21:06 -0500

    Changed app.js for new interface

--- app.js
+++ app.js
@@ -4,13 +4,72 @@ var io = require('socket.io'),
 
 var port = process.env.PORT || 3000;
 var app = connect().use(connect.static('public')).listen(port);
+
+// Chatroom
+
+var numUsers = 0;
 var chat_room = io.listen(app);
 
 chatter.set_sockets(chat_room.sockets);
 
 chat_room.sockets.on('connection', function (socket) {
-  chatter.connect_chatter({
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  /*chatter.connect_chatter({
     socket: socket,
     username: socket.id
+  });*/
+});
+
+ // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
   });
 });
